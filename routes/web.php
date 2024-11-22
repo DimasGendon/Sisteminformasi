93436c0b38ef5e8fbb75d52cebf8c008677ea9aa
ardolion <ardolion200@gmail.com>
2024-11-22 09:42:21 +0700

    rapikan web

--- routes/web.php
+++ routes/web.php
@@ -22,36 +22,20 @@ use App\Http\Controllers\SlideController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); // Untuk daftar menu
 
-
+//Login
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'store'])->name('store.login');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// Route::get('/admin', function () {
-//     return view('layout.admin');
-// });
+//Guest
+Route::get('/guest', [GuestController::class, 'index'])->name(name: 'guest');
+Route::get('/guest/single//{id}', [GuestController::class, 'showSingle_data'])->name(name: 'showSingle_data.guest');
+Route::get('/guest/multiple/{id}', [GuestController::class, 'showMultiple_data'])->name(name: 'showMultiple_data.guest');
 
-// Route::get('/user', function () {
-//     return view('layout.user');
-    Route::get('/guest', [GuestController::class, 'index'])->name(name: 'guest');
-    Route::get('/guest/single//{id}', [GuestController::class, 'showSingle_data'])->name(name: 'showSingle_data.guest');
-    Route::get('/guest/multiple/{id}', [GuestController::class, 'showMultiple_data'])->name(name: 'showMultiple_data.guest');
-
-// });
-
-Route::get('/image/{id}', [ImageController::class, 'index'])->name('image.index')->middleware('auth');
-Route::get('image/create/{id}', [ImageController::class, 'create'])->name('image.create')->middleware('auth');
-Route::post('/image', [ImageController::class, 'store'])->name('image.store')->middleware('auth');
-Route::get('/image/{id}/edit', [ImageController::class, 'edit'])->name('image.edit')->middleware('auth');
-Route::put('/image/{id}', [ImageController::class, 'update'])->name('image.update')->middleware('auth');
-Route::delete('/destroy/{id}', [ImageController::class, 'destroy'])->name('image.destroy')->middleware('auth');
-
-
-
-
-
+//Menu
 Route::get('/menu', [MenuController::class, 'index'])->name('menu.index')->middleware('auth'); // Untuk daftar menu
 Route::get('/createmenu', [MenuController::class, 'create'])->name('menu.create')->middleware('auth'); // Untuk membuka form tambah menu
 Route::post('/post', [MenuController::class, 'store'])->name('menu.store')->middleware('auth'); // Untuk menyimpan menu baru
@@ -62,6 +46,7 @@ Route::delete('/menu/{menu}', [MenuController::class, 'destroy'])->name('menu.de
 Route::post('/images', [EditorController::class, 'editor_image'])->name('store.image')->middleware('auth');
 Route::get('/multiple/{id}', [MenuController::class, 'showMultiple'])->name('multiple.index')->middleware('auth'); // Untuk menghapus menu
 
+//Multipe Data
 // Route::get('/multiple/{menu}', [MultipleController::class, 'index'])->name('multiple.index')->middleware('auth'); // Untuk daftar menu
 Route::get('/create/{menu}', [MultipleController::class, 'create'])->name('multiple.create')->middleware('auth');
 Route::post('/postmultiple', [MultipleController::class, 'store'])->name('multiple.store')->middleware('auth'); // Untuk menyimpan menu baru
@@ -70,10 +55,15 @@ Route::get('/multiple/{id}/edit', [MultipleController::class, 'edit'])->name('mu
 Route::put('/multipost/{id}', [MultipleController::class, 'update'])->name('multiple.update')->middleware('auth');
 Route::delete('/multiple/{menu}', [MultipleController::class, 'destroy'])->name('multiple.hapus')->middleware('auth'); // Untuk menghapus menu
 
+//Image
+Route::get('/image/{id}', [ImageController::class, 'index'])->name('image.index')->middleware('auth');
+Route::get('image/create/{id}', [ImageController::class, 'create'])->name('image.create')->middleware('auth');
+Route::post('/image', [ImageController::class, 'store'])->name('image.store')->middleware('auth');
+Route::get('/image/{id}/edit', [ImageController::class, 'edit'])->name('image.edit')->middleware('auth');
+Route::put('/image/{id}', [ImageController::class, 'update'])->name('image.update')->middleware('auth');
+Route::delete('/destroy/{id}', [ImageController::class, 'destroy'])->name('image.destroy')->middleware('auth');
+
 //Slide
 Route::get('/slide', [SlideController::class, 'index'])->name('slide')->middleware('auth');
 Route::post('/slide', [SlideController::class, 'store'])->name('store.slide')->middleware('auth');
 Route::delete('slide/{id}', [SlideController::class, 'destroy'])->name('slide.destroy');
-
-
-
