bab6395111b7230b3f50b6df198f73c709d0dfa5
DimasGendon <165109791+DimasGendon@users.noreply.github.com>
2024-12-16 15:20:28 +0700

    w

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -109,34 +109,11 @@
 
     <!-- End Header Area -->
 
-    {{-- @include('guest.home') --}}
+  
 
     <!-- Slider Area -->
     @include('guest.dashboardslide')
-    {{-- <section class="slider">
-        <div class="hero-slider owl-carousel owl-theme owl-loaded">
-            <!-- Start Single Slider -->
-            @foreach ($slides as $slide)
-                <img src="{{ asset('storage/' . $slide->photo_path) }}" class="single-slider">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-7">
-                            <div class="text">
-                                <h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl
-                                    pellentesque, faucibus libero eu, gravida quam. </p>
-                                <div class="button">
-                                    <a href="#" class="btn">Get Appointment</a>
-                                    <a href="#" class="btn primary">Learn More</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-            <!-- End Single Slider -->
-        </div>
-    </section>  --}}
+   
     <!--/ End Slider Area -->
 
     <!-- Start Feautes -->
